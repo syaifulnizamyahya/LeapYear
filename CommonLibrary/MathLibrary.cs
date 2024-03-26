3f5e33e1a8e4845a7cccb80b22fd68060cd405e0
syaifulnizamyahya <syaifulnizamyahya@gmail.com>
2024-03-26 14:48:23 +0800

    first commit

---
+++ CommonLibrary/MathLibrary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLibrary
+{
+    public class MathLibrary
+    {
+        public static List<int> GetLeapYears(int start, int end)
+        {
+            var leapYears = new List<int>();
+            int jump = 1;
+            for (var year = start; year <= end; year += jump)
+            {
+                if (IsLeapYear(year))
+                {
+                    leapYears.Add(year);
+                    jump = (jump != 4) ? 4 : jump;
+                }
+            }
+            return leapYears;
+        }
+        
+        public static bool IsLeapYear(int year)
+        {
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ LibraryTest/MathLibraryTest.cs
@@ -0,0 +1,46 @@
+﻿using CommonLibrary;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryTest
+{
+    [TestFixture]
+    public class MathLibraryTest
+    {
+        [OneTimeSetUp]
+        public void StartTest()
+        {
+            Trace.Listeners.Add(new ConsoleTraceListener());
+        }
+
+        [OneTimeTearDown]
+        public void EndTest()
+        {
+            Trace.Flush();
+        }
+
+        private static readonly object[] _yearRange =
+        {
+            new object[] {2000, 2005, new List<int> { 2000, 2004 } },
+            new object[] {2090, 2110, new List<int> { 2092, 2096, 2104, 2108 } },
+            new object[] {1900, 1905, new List<int> { 1904 } }
+        };
+
+        [TestCaseSource(nameof(_yearRange))]
+        public void GetLeapYears_YearsStartEnd_ReturnLeapYears(int start, int end, List<int> expected)
+        {
+            // Arrange 
+
+            // Act
+            var actual = MathLibrary.GetLeapYears(start, end);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+    }
+}
